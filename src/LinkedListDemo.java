7f0af9d95e85a87cfde8ecaa573ed8497c28063a
Alexandr <alx.kudin@gmail.com>
2020-07-20 18:01:05 +0300

    add LinkedListDemo

---
+++ src/LinkedListDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("value1");
+        a.add("value2");
+        System.out.println(a);
+    }
+}
